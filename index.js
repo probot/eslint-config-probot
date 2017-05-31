f361a98a6cb5f33f5d6d9759076393b1b2722f63
Brandon Keepers <bkeepers@github.com>
2017-05-31 08:27:27 -0500

    Comment on camelcase

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: 'xo/esnext',
   rules: {
     indent: [2, 2, {SwitchCase: 1}],
+    // Because a lot of GitHub payloads include underscores
     camelcase: 1,
     'no-else-return': 0,
     'key-spacing': 0
