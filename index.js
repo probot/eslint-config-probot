cc91c1dd0e584c38033e0fcc88a75abe7a5ab74c
Brandon Keepers <bkeepers@github.com>
2017-05-31 08:26:25 -0500

    Hide my shame

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
-	extends: 'xo/esnext',
-	rules: {
-		indent: [2, 2, {SwitchCase: 1}],
+  extends: 'xo/esnext',
+  rules: {
+    indent: [2, 2, {SwitchCase: 1}],
     camelcase: 1,
     'no-else-return': 0,
     'key-spacing': 0
